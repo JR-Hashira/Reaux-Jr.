083301df648c3ef08542b8872e585968a4e3309f
JR-Hashira <113061197+JR-Hashira@users.noreply.github.com>
2022-10-05 13:05:01 -0700

    Update jr-CODE (1).js

--- jr-CODE (1).js
+++ jr-CODE (1).js
@@ -8,40 +8,33 @@ function displayAlert(alertText){
         alert(alertText+i);
     }
     // Another Countdown Varible is any letter but just have it consist
-    //
-    for(i=10; i > 0; i--){
-        alert(alertText + "minus" +i);
+    
     }
-}
-
 
 // Runtime is where it's stores my countdown sequence
 function runtime(x) {   
 //This is the start of JavaScript Comment
 CurrTime = 50;
 //The number my timer starts at
-var Timeout = 5000;
+var Timeout = 1000;
 //How long before each change in the timer, 5000 miliseconds
-
+for(i=0; i < 11; i++){
 setTimeout(function(){
-x.innerHTML = CurrTime;
-Currtime = Currtime - 5;
 //Currtime will be decreasing by 5 seconds each time
-}, setTimeout(function(){
     if(CurrTime == 0){
-    x.innerHTML = Currtime
+    x.innerHTML = CurrTime
     alert("Testing the Charge")
+    // When my timer his zero, the alert will show
 }
 else if(CurrTime < 25){
     x.innerHTML = "Want a break from the charge?"
+    // When timer hits less than 25 seconds an alert will show
 }
 else{
     x.innerHTML = CurrTime}
+    CurrTime = CurrTime - 5;
 
-setTimeout(function(){
-x.innerHTML = CurrTime;
-//Lines 18 Throught 75 is the whole countdown 
-// The Alert for when my time equals zero
-alert("Blasting off again");
 }, Timeout);
-
+Timeout = Timeout + 1000;
+}
+}
